19acc712c8fa34796d2018d01890880b5b82b804
lgmacedo <lucasguerra.macedo@gmail.com>
2023-05-12 11:14:20 -0300

    fix: Correct deployment environment variable

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
   console.log(`Server running on port ${PORT}`)
--- src/database/database.connect.js
+++ src/database/database.connect.js
@@ -4,4 +4,7 @@ dotenv.config();
 
 const { Pool } = pg;
 
-export const db = new Pool({connectionString: process.env.DATABASE_URL});
+export const db = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
